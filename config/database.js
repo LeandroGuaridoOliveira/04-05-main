a369ef8f3fda2908c2f52d341fb89172beb294cb
LeandroGuaridoOliveira <84139383+LeandroGuaridoOliveira@users.noreply.github.com>
2022-05-09 13:16:49 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const conexao = async() => {
+    //conexao local
+    //var bdlocal = await mongoose.connect('mongodb://localhost/ac1tri')
+    //conexao atlas
+    var atlas = await mongoose.connect('mongodb+srv://UserAdmin:rm14133@ac1-2tri.xb0bm.mongodb.net/test')
+
+    
+}
+
+module.exports = conexao
